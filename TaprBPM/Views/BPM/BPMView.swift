483218d84cd64cbf184af117b269ff57e58bf38b
Liam Byrne <liam.jb1@gmail.com>
2025-04-11 19:18:26 +0100

    Simplify BPMView structure

--- TaprBPM/Views/BPM/BPMView.swift
+++ TaprBPM/Views/BPM/BPMView.swift
@@ -21,9 +21,12 @@ import SwiftUI
 struct BPMView: View {
     @ObservedObject var viewModel: BPMViewModel
 
+    @State var feedbackAnimationTrigger = false
+    @State var resetAnimationTrigger = false
+
     var body: some View {
         ZStack {
-            BackgroundView(
+            backgroundView(
                 color: viewModel.backgroundColor
             )
             VStack {
@@ -38,13 +41,14 @@ struct BPMView: View {
         }
         .ignoresSafeArea()
         .simultaneousGesture(
-            TapGesture()
-                .onEnded {
-                    viewModel.tapReceived()
-                }
+            TapGesture().onEnded {
+                feedbackAnimationTrigger.toggle()
+                viewModel.tapReceived()
+            }
         )
         .simultaneousGesture(
             LongPressGesture(minimumDuration: 0.3).onEnded { _ in
+                resetAnimationTrigger.toggle()
                 Task {
                     try await Task.sleep(for: .seconds(0.7))
                     viewModel.reset()
@@ -53,79 +57,65 @@ struct BPMView: View {
         )
     }
 
-    struct BackgroundView: View {
-        struct AnimationValues {
-            var opacity = 0.0
-            var scale = 0.2
+    private func backgroundView(color: Color) -> some View {
+        ZStack {
+            color
+            feedbackAnimationView
+            resetAnimationView
         }
+    }
 
-        let color: Color
+    // MARK: - Animations
 
-        @State var feedbackAnimationTrigger = false
-        @State var resetAnimationTrigger = false
+    struct AnimationValues {
+        var opacity = 0.0
+        var scale = 0.2
+    }
 
-        var body: some View {
-            ZStack {
-                color
-                    .onTapGesture {
-                        feedbackAnimationTrigger.toggle()
-                    }
-                    .simultaneousGesture(
-                        LongPressGesture(minimumDuration: 0.3)
-                            .onEnded { _ in
-                                resetAnimationTrigger.toggle()
-                            }
-                    )
-                feedbackAnimationView
-                resetAnimationView
+   private var feedbackAnimationView: some View {
+        Circle()
+            .foregroundStyle(.gray)
+            .keyframeAnimator(
+                initialValue: AnimationValues(),
+                trigger: feedbackAnimationTrigger
+            ) { content, value in
+                content
+                    .opacity(value.opacity)
+                    .scaleEffect(value.scale)
+            } keyframes: { _ in
+                KeyframeTrack(\.opacity) {
+                    LinearKeyframe(0.1, duration: 0.1)
+                    LinearKeyframe(0.0, duration: 0.4)
+                }
+                KeyframeTrack(\.scale) {
+                    LinearKeyframe(3.0, duration: 0.4)
+                    LinearKeyframe(1.0, duration: 0.1)
+                }
             }
-        }
+    }
 
-        private var feedbackAnimationView: some View {
-            Circle()
-                .foregroundStyle(.gray)
-                .keyframeAnimator(
-                    initialValue: AnimationValues(),
-                    trigger: feedbackAnimationTrigger
-                ) { content, value in
-                    content
-                        .opacity(value.opacity)
-                        .scaleEffect(value.scale)
-                } keyframes: { _ in
-                    KeyframeTrack(\.opacity) {
-                        LinearKeyframe(0.1, duration: 0.1)
-                        LinearKeyframe(0.0, duration: 0.4)
-                    }
-                    KeyframeTrack(\.scale) {
-                        LinearKeyframe(3.0, duration: 0.4)
-                        LinearKeyframe(1.0, duration: 0.1)
-                    }
+    private var resetAnimationView: some View {
+        Circle()
+            .foregroundStyle(.white)
+            .keyframeAnimator(
+                initialValue: AnimationValues(),
+                trigger: resetAnimationTrigger
+            ) { content, value in
+                content
+                    .opacity(value.opacity)
+                    .scaleEffect(value.scale)
+            } keyframes: { _ in
+                KeyframeTrack(\.opacity) {
+                    LinearKeyframe(0.5, duration: 0.1)
+                    LinearKeyframe(1.0, duration: 1.0)
+                    LinearKeyframe(0.0, duration: 0.1)
                 }
-        }
-
-        private var resetAnimationView: some View {
-            Circle()
-                .foregroundStyle(.white)
-                .keyframeAnimator(
-                    initialValue: AnimationValues(),
-                    trigger: resetAnimationTrigger
-                ) { content, value in
-                    content
-                        .opacity(value.opacity)
-                        .scaleEffect(value.scale)
-                } keyframes: { _ in
-                    KeyframeTrack(\.opacity) {
-                        LinearKeyframe(0.5, duration: 0.1)
-                        LinearKeyframe(1.0, duration: 1.0)
-                        LinearKeyframe(0.0, duration: 0.1)
-                    }
-                    KeyframeTrack(\.scale) {
-                        LinearKeyframe(1.0, duration: 0.1)
-                        LinearKeyframe(4.0, duration: 1.0)
-                        LinearKeyframe(1.0, duration: 0.1)
-                    }
+                KeyframeTrack(\.scale) {
+                    LinearKeyframe(1.0, duration: 0.1)
+                    LinearKeyframe(4.0, duration: 1.0)
+                    LinearKeyframe(1.0, duration: 0.1)
                 }
-        }
+            }
     }
 }
 
